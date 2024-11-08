58534fab18b33e4c922fa8e0a685754f35a23921
Ilia Markovski <i.markovski@jdm.de>
2024-11-09 00:58:38 +0200

    add google login with firebase

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,38 +1,64 @@
 import { GoogleOutlined } from '@ant-design/icons';
 import styles from './LoginPage.module.scss';
 import { Button } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { app } from '../../../firebaseConfig';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+} from 'firebase/auth';
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const LoginPage = () => {
-  const [loadings, setLoadings] = useState<boolean[]>([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const enterLoading = (index: number) => {
-    setLoadings(prevLoadings => {
-      const newLoadings = [...prevLoadings];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
-
-    setTimeout(() => {
-      setLoadings(prevLoadings => {
-        const newLoadings = [...prevLoadings];
-        newLoadings[index] = false;
-        return newLoadings;
-      });
-    }, 6000);
+  const handleLogin = () => {
+    setLoading(true);
+    signInWithPopup(auth, provider)
+      .then(result => {
+        setUser(result.user);
+        console.log(result.user);
+      })
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+  };
+  const handleLogout = () => {
+    setUser(null);
+    auth.signOut();
   };
 
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
   return (
     <div className={styles.container}>
-      <Button
-        type='primary'
-        size='large'
-        icon={<GoogleOutlined />}
-        loading={loadings[1]}
-        onClick={() => enterLoading(1)}
-      >
-        Login with Google
-      </Button>
+      {!user ? (
+        <Button
+          type='primary'
+          size='large'
+          icon={<GoogleOutlined />}
+          loading={loading}
+          onClick={() => handleLogin()}
+        >
+          Login with Google
+        </Button>
+      ) : null}
+
+      {user ? (
+        <Button
+          type='primary'
+          size='large'
+          loading={loading}
+          onClick={() => handleLogout()}
+        >
+          Logout
+        </Button>
+      ) : null}
     </div>
   );
 };
